079269592d75f2a19ae804c3ff9e8ea62f154223
Francesco Gatti <gattifrancesco@hotmail.it>
2017-02-24 02:28:44 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,49 @@ from skvideo.io import VideoCapture
 #from cv2 import VideoCapture
 
 def compute_lines(image):
-	return image
+    gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+
+    # Define a kernel size and apply Gaussian smoothing
+    kernel_size = 5
+    blur_gray = cv2.GaussianBlur(image,(kernel_size, kernel_size),0)
+
+    # Define our parameters for Canny and apply
+    low_threshold = 50
+    high_threshold = 100
+    edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+    # Next we'll create a masked edges image using cv2.fillPoly()
+    mask = np.zeros_like(edges)   
+
+    # This time we are defining a four sided polygon to mask
+    imshape = image.shape
+    marg = imshape[1]/2.5
+    h = imshape[0]/1.6
+    vertices = np.array([\
+        [(0,imshape[0]),(marg, h), (imshape[1] - marg, h), (imshape[1],imshape[0])]], dtype=np.int32)
+    
+    cv2.fillPoly(mask, vertices, 255)
+    masked_edges = cv2.bitwise_and(edges, mask)
+
+    # Define the Hough transform parameters
+    # Make a blank the same size as our image to draw on
+    rho = 1
+    theta = np.pi/180
+    threshold = 10
+    min_line_length = 10
+    max_line_gap = 1
+
+    # Run Hough on edge detected image
+    lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
+                                min_line_length, max_line_gap)
+
+    # Iterate over the output "lines" and draw lines on the blank
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(image,(x1,y1),(x2,y2),(0,0,255,100),5)
+    
+    image = cv2.addWeighted(image, 0.8, cv2.cvtColor(mask,cv2.COLOR_GRAY2RGB), 0.2, 0)
+    return image
 
 
 cap = VideoCapture("test.avi")
